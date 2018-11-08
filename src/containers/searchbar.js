98b85982a7e46688978389f639706eb9b735bbe3
ParkMD7 <parkermdinsmore@gmail.com>
2018-11-08 17:18:31 -0500

    switched searchbar from class to functional component

--- src/containers/searchbar.js
+++ src/containers/searchbar.js
@@ -1,35 +1,27 @@
 // dependencies
-import React, { Component } from "react";
+import React from "react";
 import { Input, Button } from 'semantic-ui-react';
 
-class SearchBar extends Component {
-    // state = {
-    //   term: ""
-    // }
+const SearchBar = () => {
 
-
-  onInputChange = (event) => {
-    this.props.search(event.target.value)
+  const onInputChange = (event) => {
+    props.search(event.target.value)
   }
 
-  onFormSubmit = (event) => {
+  const onFormSubmit = (event) => {
     event.preventDefault();
-    // this.setState({ term: "" });
   }
 
-  render() {
     return (
       <form onSubmit={this.onFormSubmit} className="input-group">
         <Input
           placeholder="Filter Goals"
           size='medium'
-          // value={this.state.term}
           onChange={this.onInputChange}
           style={{width: '300px'}}
         />
       </form>
     );
-  }
 }
 
 
